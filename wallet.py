0c1dc4c6511c290b3570c7353a6ebcf98cfd5e0d
RaulAlvz04 <rasty04ra@gmail.com>
2025-11-11 15:41:59 +0100

    Fix typo in get_balance method

--- wallet.py
+++ wallet.py
@@ -6,7 +6,7 @@ class Wallet:
         self.balance = self.balance + val
 
     def get_balance(self):
-        return sef.balance
+        return self.balance
 
     def remove_balance(self, val):
         self.balance = self.balance - val
